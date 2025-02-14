2bff6c884072ab9a5343726a024e973e86c25fcd
lobch <llobchevskaya@gmail.com>
2025-02-14 20:52:10 +0300

    ready task3

---
+++ src/main/java/ru/aston/lobchevskaya_eyu/task3/StreamAPITask.java
@@ -0,0 +1,8 @@
+package ru.aston.lobchevskaya_eyu.task3;
+
+public class StreamAPITask {
+
+    public static void main(String[] args) {
+        System.out.println("Задание сделано в тестах");
+    }
+}
